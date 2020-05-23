1327c50ed54351034caac167419cb29ac0a018b7
sherif <sherifyounuse@gmail.com>
2020-05-23 22:37:48 +0530

    updated

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
